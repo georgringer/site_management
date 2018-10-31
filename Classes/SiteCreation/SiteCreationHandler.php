022dd6e1ce5d9a9b660e83d52c6dd9e343fcdc3f
Georg Ringer <georg.ringer@gmail.com>
2018-10-31 19:04:30 +0100

    [TASK] Use DepenencyResolver for steps

--- Classes/SiteCreation/SiteCreationHandler.php
+++ Classes/SiteCreation/SiteCreationHandler.php
@@ -6,6 +6,8 @@ namespace GeorgRinger\SiteManagement\SiteCreation;
 use GeorgRinger\SiteManagement\Domain\Model\Dto\Configuration;
 use GeorgRinger\SiteManagement\Domain\Model\Dto\Response;
 use GeorgRinger\SiteManagement\SiteCreation\Step\SiteCreationInterface;
+use TYPO3\CMS\Core\Package\PackageManager;
+use TYPO3\CMS\Core\Service\DependencyOrderingService;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 class SiteCreationHandler
@@ -14,12 +16,20 @@ class SiteCreationHandler
     /** @var Configuration */
     protected $configuration;
 
+    /** @var PackageManager */
+    protected $packageManager;
+
+    /** @var DependencyOrderingService */
+    protected $dependencyOrderingService;
+
     /**
      * @param Configuration $configuration
      */
     public function __construct(Configuration $configuration)
     {
         $this->configuration = $configuration;
+        $this->packageManager = GeneralUtility::makeInstance(PackageManager::class);
+        $this->dependencyOrderingService = GeneralUtility::makeInstance(DependencyOrderingService::class);
     }
 
 
@@ -27,23 +37,62 @@ class SiteCreationHandler
     {
         $response = GeneralUtility::makeInstance(Response::class);
 
-        $configuration = $this->configuration;
+        $allConfiguredSteps = $this->loadConfiguration();
+        $allSteps = $this->sanitizeSteps($allConfiguredSteps);
 
-        foreach ($this->getSteps() as $class => $stepConfiguration) {
+        foreach ($allSteps as $name => $stepConfiguration) {
             /** @var SiteCreationInterface $step */
-            $step = GeneralUtility::makeInstance($class);
+            $step = GeneralUtility::makeInstance($stepConfiguration['target']);
             if ($step->isValid()) {
                 $options = $stepConfiguration['options'] ?? [];
-                $step->handle($configuration, $response, $options);
+                $step->handle($this->configuration, $response, $options);
             }
         }
 
         return $response;
     }
 
-    protected function getSteps(): array
+    /**
+     * Loop over all packages and check for a Configuration/SiteCreationSteps.php file
+     *
+     * @return array
+     */
+    protected function loadConfiguration(): array
     {
-        return $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['site_management']['steps'];
+        $packages = $this->packageManager->getActivePackages();
+        $allSteps = [[]];
+        foreach ($packages as $package) {
+            $packageConfiguration = $package->getPackagePath() . 'Configuration/SiteCreationSteps.php';
+            if (file_exists($packageConfiguration)) {
+                $stepConfiguration = require $packageConfiguration;
+                if (is_array($stepConfiguration)) {
+                    $allSteps[] = $stepConfiguration;
+                }
+            }
+        }
+        return array_replace_recursive(...$allSteps);
     }
 
+    /**
+     * Order and sanizize steps
+     *
+     * @param array
+     * @return array
+     */
+    protected function sanitizeSteps(array $allSteps): array
+    {
+        $steps = [];
+        foreach ($allSteps as $stepsOfStack) {
+            foreach ($stepsOfStack as $name => $step) {
+                // Skip this step if disabled by configuration
+                if (isset($step['disabled']) && $step['disabled'] === true) {
+                    continue;
+                }
+                $steps[$name] = $step;
+            }
+        }
+
+        $steps = $this->dependencyOrderingService->orderByDependencies($steps);
+        return $steps;
+    }
 }
---
+++ Configuration/SiteCreationSteps.php
@@ -0,0 +1,46 @@
+<?php
+
+return [
+    'site_management' => [
+        'typo3/ext-site_management/copy-pagetree' => [
+            'target' => \GeorgRinger\SiteManagement\SiteCreation\Step\CopyPageTree::class,
+            'options' => []
+        ],
+        'typo3/ext-site_management/create-site-configuration' => [
+            'target' => \GeorgRinger\SiteManagement\SiteCreation\Step\CreateSiteConfiguration::class,
+            'after' => [
+                'typo3/ext-site_management/copy-pagetree',
+            ]
+        ],
+        'typo3/ext-site_management/reset-root-page' => [
+            'target' => \GeorgRinger\SiteManagement\SiteCreation\Step\ResetRootPage::class,
+            'after' => [
+                'typo3/ext-site_management/create-site-configuration',
+            ]
+        ],
+        'typo3/ext-site_management/create-sysfilemounts' => [
+            'target' => \GeorgRinger\SiteManagement\SiteCreation\Step\CreateSysFileMounts::class,
+            'after' => [
+                'typo3/ext-site_management/reset-root-page',
+            ]
+        ],
+        'typo3/ext-site_management/create-usergroups' => [
+            'target' => \GeorgRinger\SiteManagement\SiteCreation\Step\CreateUserGroup::class,
+            'after' => [
+                'typo3/ext-site_management/create-sysfilemounts',
+            ]
+        ],
+        'typo3/ext-site_management/create-users' => [
+            'target' => \GeorgRinger\SiteManagement\SiteCreation\Step\CreateUsers::class,
+            'after' => [
+                'typo3/ext-site_management/create-usergroups',
+            ]
+        ],
+        'typo3/ext-site_management/send-mail' => [
+            'target' => \GeorgRinger\SiteManagement\SiteCreation\Step\SendMail::class,
+            'after' => [
+                'typo3/ext-site_management/create-users',
+            ]
+        ],
+    ]
+];
