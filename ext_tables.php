7e1379fd6f9a716bf04ed4e984d1a3be5f3e0bcb
Georg Ringer <georg.ringer@gmail.com>
2018-10-31 19:10:10 +0100

    [TASK] Use DepenencyResolver for steps

--- ext_tables.php
+++ ext_tables.php
@@ -10,7 +10,7 @@ defined('TYPO3_MODE') or die();
         'routeTarget' => \GeorgRinger\SiteManagement\Controller\SiteManagementController::class . '::handleRequest',
         'access' => 'admin',
         'name' => 'site_management',
-        'icon' => 'EXT:site_management/Resources/Public/Icons/Extension.png',
+        'icon' => 'EXT:site_management/Resources/Public/Icons/module-site-management.svg',
         'labels' => 'LLL:EXT:site_management/Resources/Private/Language/locallang_sitemanagement_module.xlf'
     ]
 );
