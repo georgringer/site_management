05994418771a5225b1708d2b01eba63a88e7eea4
Georg Ringer <georg.ringer@gmail.com>
2018-11-06 20:57:08 +0100

    [BUGFIX] Crrect ext_tables.sql

--- ext_tables.sql
+++ ext_tables.sql
@@ -6,19 +6,19 @@ CREATE TABLE pages
 
 CREATE TABLE be_users
 (
-  tx_site_management_site     int(11) unsigned DEFAULT '0' NOT NULL,
-  tx_site_management_based_on int(11) unsigned DEFAULT '0' NOT NULL,
+  tx_site_management_site     int(11) unsigned DEFAULT 0 NOT NULL,
+  tx_site_management_based_on int(11) unsigned DEFAULT 0 NOT NULL,
 );
 
 
 CREATE TABLE be_groups
 (
-  tx_site_management_site     int(11) unsigned DEFAULT '0' NOT NULL,
-  tx_site_management_based_on int(11) unsigned DEFAULT '0' NOT NULL,
+  tx_site_management_site     int(11) unsigned DEFAULT 0 NOT NULL,
+  tx_site_management_based_on int(11) unsigned DEFAULT 0 NOT NULL,
 );
 
 CREATE TABLE sys_filemounts
 (
-  tx_site_management_site     int(11) unsigned DEFAULT '0' NOT NULL,
-  tx_site_management_based_on int(11) unsigned DEFAULT '0' NOT NULL,
+  tx_site_management_site     int(11) unsigned DEFAULT 0 NOT NULL,
+  tx_site_management_based_on int(11) unsigned DEFAULT 0 NOT NULL,
 );
